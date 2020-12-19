75151ff064ff014ace654be223c5e838fde2befe
yogen31 <yogen.bhattarai@gmail.com>
2020-12-19 15:56:59 +0545

    new test project

---
+++ src/component/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Main.scss";
+
+function Main() {
+    return (
+        <div className="main">
+        <p>hello my name is yogendra</p>
+        <button className="main-button">clickMe</button>
+            
+        </div>
+    )
+}
+
+export default Main
