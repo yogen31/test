d9163df1c1857062cefdb5e4d933d9032d760325
yogen31 <yogen.bhattarai@gmail.com>
2020-12-20 21:36:14 +0545

    responsive made

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,14 +1,62 @@
-import React from 'react';
-import "./Main.scss";
+import React from "react";
+import "./Main.css";
 
 function Main() {
-    return (
-        <div className="main">
-        <p>hello my name is yogendra</p>
-        <button className="main-button">clickMe</button>
-            
+  const [className, setClassName] = React.useState(false);
+
+  const handleClick = () => {
+    setClassName(true);
+  };
+  return (
+    <>
+      <nav className="navBar">
+        <div className="navBar-item">
+          <i class="fa fa-tachometer" aria-hidden="true"></i>
         </div>
-    )
+
+        <div className="navBar-item navBar-item2">
+          <div
+            style={className ? { display: "none" } : { cursor: "pointer" }}
+            className={className}
+            onClick={handleClick}
+          >
+            <i className="fa fa-window-close-o " aria-hidden="true"></i>
+          </div>
+          <div>
+            <i className="fa fa-bell-o" aria-hidden="true"></i>
+            <i className="fa fa-github margin-x" aria-hidden="true"></i>
+            <i className="fa fa-user" aria-hidden="true"></i>
+          </div>
+          <div>
+            <span
+              class="open"
+              style={className ? {} : { display: "none" }}
+              onClick={() => {
+                setClassName(false);
+              }}
+            >
+              open
+            </span>
+          </div>
+        </div>
+      </nav>
+
+      <section className="main">
+        <div
+          className="main-item "
+          style={className ? { border: "0", width: "100%" } : {}}
+        >
+          One
+        </div>
+        <div
+          className="main-item avinaya"
+          style={className ? { display: "none" } : {}}
+        >
+          Two
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Main
+export default Main;
